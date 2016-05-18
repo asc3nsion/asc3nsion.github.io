8a44b417f945312fc78742d9cdfa3ea0a7792c27
Jason Norgaar <Jason Norgaar>
2016-05-18 14:35:11 -0700

    test url

--- geoji/scripts/app.js
+++ geoji/scripts/app.js
@@ -52,7 +52,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   if (window.location.port === '') { // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    app.baseUrl = '/geoji/test/test/';
+    app.baseUrl = '/geoji/';
   }
 
   //user data
